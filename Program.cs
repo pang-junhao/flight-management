cd5ae3ed0bda28d90c9072b2f42dec2a8f2c91e4
Pang Jun Hao <junhao2234@gmail.com>
2026-07-01 02:27:29 +0800

    Add initial source code (Flight Management System)

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Responsible to start the program
+using System;
+namespace CustomProgram
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FlightSystem system = new FlightSystem();
+            
+            system.Run();
+        }
+    }
+}
